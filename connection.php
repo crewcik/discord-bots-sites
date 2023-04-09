7bac264766a3bbe31b47e6fd6555f29428196fe5
CrewLua <113601819+CrewLua@users.noreply.github.com>
2023-04-10 02:39:19 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+
+connection_status="Hesap verileriniz yükleniyor."
+constant="Sunucu verileriniz yükleniyor."
+
+locale_get_display_script(connection_status, constant)
+
+php?>
---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.multiple-text', {
+    strings: ["Raxe", "Tamamen türkçe", "Kullanımı kolay panaller", "Gelişmiş sistemler", "/yardım", "/cinsiyetli-kayıt", "/normal-kayıt", "/rank", "Ve daha fazlası tek bir botta!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
